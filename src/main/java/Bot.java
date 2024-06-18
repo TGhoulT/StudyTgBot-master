d08240ef58476cce074ac78d2cc6cc7a3e622046
TGhoulT <mgenius012975@gmail.com>
2024-06-18 11:29:21 +0300

    Amazing v1.8.2

--- src/main/java/Bot.java
+++ src/main/java/Bot.java
@@ -1,6 +1,7 @@
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.AnswerCallbackQuery;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.methods.updatingmessages.EditMessageReplyMarkup;
 import org.telegram.telegrambots.meta.api.methods.updatingmessages.EditMessageText;
 import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
@@ -17,10 +18,9 @@ public class Bot extends TelegramLongPollingBot {
     private final List<Question> questions = new ArrayList<>();
     private final Map<Long, Integer> userScores = new HashMap<>();
     private final Map<Long, Integer> userProgress = new HashMap<>();
-    private final Map<Long, Integer> userMessages = new HashMap<>();
     private final Map<Long, Boolean> waitingForCandidateInfo = new HashMap<>();
     private final DBManager dbManager = new DBManager();
-    private static final String ADMIN_CHAT_ID = "your_chat_id";
+    private static final String ADMIN_CHAT_ID = "-1002232543668";
 
     public Bot() {
         initializeQuestions();
@@ -49,14 +49,142 @@ public class Bot extends TelegramLongPollingBot {
         ));
     }
 
+    public InlineKeyboardButton back = InlineKeyboardButton.builder()
+            .text("Назад")
+            .callbackData("назад")
+            .build();
+    public InlineKeyboardButton downloadIntellijIdea = InlineKeyboardButton.builder()
+            .text("Скачать среду разработки")
+            .callbackData("скачать среду разработки")
+            .url("https://www.jetbrains.com/ru-ru/idea/download/?section=windows")
+            .build();
+
+    public InlineKeyboardButton jarFile = InlineKeyboardButton.builder()
+            .text("Упаковка jar-файла")
+            .callbackData("упаковка jar-файла")
+            .url("https://docs.google.com/document/d/1YM1mylggI9d1ZkxenWfHRGxn_NK38FJ-1JrHj2eeR1E/edit#heading=h.9yiytdaelm8b")
+            .build();
+
+    public InlineKeyboardButton encapsulation = InlineKeyboardButton.builder()
+            .text("Инкапсуляция")
+            .callbackData("инкапсуляция")
+            .url("https://blog.skillfactory.ru/glossary/inkapsulyacziya/")
+            .build();
+
+    public InlineKeyboardButton pojo = InlineKeyboardButton.builder()
+            .text("POJO-классы, геттеры и сеттеры")
+            .callbackData("POJO-классы, геттеры и сеттеры")
+            .url("https://javarush.com/groups/posts/1928-getterih-i-setterih")
+            .build();
+    public InlineKeyboardButton operationWithNumbers = InlineKeyboardButton.builder()
+            .text("Операции с числами")
+            .callbackData("операции с числами")
+            .url("docs.oracle.com/cd/A58617_01/server.804/a58225/ch3all.htm")
+            .build();
+
+    public InlineKeyboardButton incrementAndDecrement = InlineKeyboardButton.builder()
+            .text("Инкремент и декремент")
+            .callbackData("инкремент и декремент")
+            .url("https://docs.oracle.com/cd/E19253-01/817-6223/chp-typeopexpr-9/index.html")
+            .build();
+
+    public InlineKeyboardButton linesConcatenationAndCompare = InlineKeyboardButton.builder()
+            .text("Строки, конкатенация и сравнение")
+            .callbackData("строки, конкатенация и сравнение")
+            .url("https://docs.oracle.com/javase/8/docs/api/java/util/Comparator.html")
+            .build();
+
+    public InlineKeyboardButton updateNumbersToLinesAndReverse = InlineKeyboardButton.builder()
+            .text("Преобразование чисел в строки и обратно")
+            .callbackData("преобразование чисел в строки и обратно")
+            .url("https://docs.oracle.com/javase/8/docs/api/java/lang/String.html#contains-java.lang.CharSequence-")
+            .build();
+
+    public InlineKeyboardButton createArrays = InlineKeyboardButton.builder()
+            .text("Создание массивов")
+            .callbackData("создание массивов")
+            .url("https://docs.oracle.com/javase/tutorial/java/nutsandbolts/arrays.html")
+            .build();
+
+    public InlineKeyboardButton workWithArraysInCycles = InlineKeyboardButton.builder()
+            .text("Работа с массивами в циклах")
+            .callbackData("работа с массивами в циклах")
+            .url("https://docs.oracle.com/javase%2F8%2Fdocs%2Fapi%2F%2F/java/util/Arrays.html")
+            .build();
+
+    public InlineKeyboardButton hashSet = InlineKeyboardButton.builder()
+            .text("Коллекция HashSet")
+            .callbackData("коллекция HashSet")
+            .url("https://docs.oracle.com/javase/8/docs/api/java/util/HashSet.html")
+            .build();
+
+    public InlineKeyboardButton treeSet = InlineKeyboardButton.builder()
+            .text("Коллекция TreeSet")
+            .callbackData("коллекция TreeSet")
+            .url("https://docs.oracle.com/javase/8/docs/api/java/util/TreeSet.html")
+            .build();
+
+    public InlineKeyboardButton fromArraysToCollectionAndReverse = InlineKeyboardButton.builder()
+            .text("Преобразование массивов и коллекций")
+            .callbackData("преобразование массивов и коллекций")
+            .url("https://docs.oracle.com/javase/8/docs/api/?java/util/Collections.html")
+            .build();
+
+    public InlineKeyboardButton iterator = InlineKeyboardButton.builder()
+            .text("Итератор")
+            .callbackData("итератор")
+            .url("https://docs.oracle.com/javase/8/docs/api/java/util/Iterator.html")
+            .build();
+
+    private InlineKeyboardMarkup sendModuleOne = InlineKeyboardMarkup.builder()
+            .keyboardRow(List.of(downloadIntellijIdea))
+            .keyboardRow(List.of(jarFile))
+            .keyboardRow(List.of(back))
+            .build();
+    private InlineKeyboardMarkup sendModuleFive = InlineKeyboardMarkup.builder()
+            .keyboardRow(List.of(encapsulation))
+            .keyboardRow(List.of(pojo))
+            .keyboardRow(List.of(back))
+            .build();
+
+    private InlineKeyboardMarkup sendModuleSeven = InlineKeyboardMarkup.builder()
+            .keyboardRow(List.of(operationWithNumbers))
+            .keyboardRow(List.of(incrementAndDecrement))
+            .keyboardRow(List.of(back))
+            .build();
+
+    private InlineKeyboardMarkup sendModuleEight = InlineKeyboardMarkup.builder()
+            .keyboardRow(List.of(linesConcatenationAndCompare))
+            .keyboardRow(List.of(updateNumbersToLinesAndReverse))
+            .keyboardRow(List.of(back))
+            .build();
+
+    private InlineKeyboardMarkup sendModuleNine = InlineKeyboardMarkup.builder()
+            .keyboardRow(List.of(createArrays))
+            .keyboardRow(List.of(workWithArraysInCycles))
+            .keyboardRow(List.of(back))
+            .build();
+
+    private InlineKeyboardMarkup sendModuleTen = InlineKeyboardMarkup.builder()
+            .keyboardRow(List.of(hashSet))
+            .keyboardRow(List.of(treeSet))
+            .keyboardRow(List.of(back))
+            .build();
+
+    private InlineKeyboardMarkup sendModuleEleven = InlineKeyboardMarkup.builder()
+            .keyboardRow(List.of(fromArraysToCollectionAndReverse))
+            .keyboardRow(List.of(iterator))
+            .keyboardRow(List.of(back))
+            .build();
+
     @Override
     public String getBotUsername() {
-        return "@your_bot";
+        return "@oijava_bot";
     }
 
     @Override
     public String getBotToken() {
-        return "YOUR_BOT_TOKEN";
+        return "7402238078:AAG6tBKAjg6i0DE2vvv7FFtwQWjmwUfX3A4";
     }
 
     @Override
@@ -73,7 +201,7 @@ public class Bot extends TelegramLongPollingBot {
         Long chatId = message.getChatId();
         if (text.equals("/test") || text.equals("/start_test")) {
             startTest(chatId);
-        } else if (text.equals("/menu")) { //функционал пока не реализован
+        } else if (text.equals("/menu")) {
             sendMenu(chatId);
         } else if (waitingForCandidateInfo.getOrDefault(chatId, false)) {
             saveCandidateInfo(message);
@@ -81,30 +209,35 @@ public class Bot extends TelegramLongPollingBot {
     }
 
     private void startTest(Long chatId) {
-        userScores.put(chatId, 0);
         userProgress.put(chatId, 0);
-        sendQuestion(chatId, 0);
-    }
+        userScores.put(chatId, 0);
 
-    private void sendQuestion(Long chatId, int questionIndex) {
-        Question question = questions.get(questionIndex);
-        InlineKeyboardMarkup keyboard = buildQuestionKeyboard(question);
+        //получаем первый вопрос из списка вопросов
+        Question firstQuestion = questions.get(0);
 
+        //создать новое сообщение с первым вопросом
         SendMessage message = SendMessage.builder()
                 .chatId(chatId.toString())
-                .text(question.getQuestion())
-                .replyMarkup(keyboard)
+                .text(firstQuestion.getQuestion())
+                .replyMarkup(createQuestionMarkup(firstQuestion))
                 .build();
-
         try {
-            Message sentMessage = execute(message);
-            userMessages.put(chatId, sentMessage.getMessageId());
+            execute(message);
         } catch (TelegramApiException e) {
             e.printStackTrace();
         }
     }
 
-    private InlineKeyboardMarkup buildQuestionKeyboard(Question question) {
+    private void startTest(Long chatId, int messageId) {
+        //обнуление прогресса и очков пользователя
+        userProgress.put(chatId, 0);
+        userScores.put(chatId, 0);
+
+        //отправка первого вопроса
+        editQuestion(chatId, 0, messageId);
+    }
+
+    private InlineKeyboardMarkup createQuestionMarkup(Question question) {
         List<List<InlineKeyboardButton>> buttons = new ArrayList<>();
         for (int i = 0; i < question.getOptions().size(); i++) {
             buttons.add(List.of(InlineKeyboardButton.builder()
@@ -121,20 +254,54 @@ public class Bot extends TelegramLongPollingBot {
         int messageId = update.getCallbackQuery().getMessage().getMessageId();
         int questionIndex = userProgress.getOrDefault(chatId, 0);
 
-        if (callbackData.matches("\\d+")) {
-            int answerIndex = Integer.parseInt(callbackData);
-            if (answerIndex == questions.get(questionIndex).getCorrectAnswerIndex()) {
-                userScores.put(chatId, userScores.get(chatId) + 1);
-            }
-
-            questionIndex++;
-            userProgress.put(chatId, questionIndex);
-
-            if (questionIndex < questions.size()) {
-                editQuestion(chatId, questionIndex);
-            } else {
-                finishTest(chatId);
-            }
+        switch (callbackData) {
+            case "/test":
+                startTest(chatId, messageId);
+                break;
+            case "/materials":
+                sendMaterialsMenu(chatId, messageId);
+                break;
+            case "вводный модуль":
+                sendMaterialDetails(chatId, messageId, "скачать среду разработки", sendModuleOne);
+                break;
+            case "инкапсуляция":
+                sendMaterialDetails(chatId, messageId, "Инкапсуляция", sendModuleFive);
+                break;
+            case "числа и даты":
+                sendMaterialDetails(chatId, messageId, "Числа и даты", sendModuleSeven);
+                break;
+            case "строки":
+                sendMaterialDetails(chatId, messageId, "Строки", sendModuleEight);
+                break;
+            case "массивы и списки":
+                sendMaterialDetails(chatId, messageId, "Массивы и списки", sendModuleNine);
+                break;
+            case "коллекции Set, Map":
+                sendMaterialDetails(chatId, messageId, "Коллекции Set, Map", sendModuleTen);
+                break;
+            case "comparator Iterator Collections":
+                sendMaterialDetails(chatId, messageId, "Comparator Iterator Collections", sendModuleEleven);
+                break;
+            case "назад":
+//                sendMaterialDetails(chatId, messageId, "Выберите материал:", keyboardM1);
+                sendMaterialsMenu(chatId, messageId);
+                break;
+            default:
+                //обработка ответов на тест
+                if (callbackData.matches("\\d+")) {
+                    int answerIndex = Integer.parseInt(callbackData);
+                    if (answerIndex == questions.get(questionIndex).getCorrectAnswerIndex()) {
+                        userScores.put(chatId, userScores.get(chatId) + 1);
+                    }
+                    questionIndex++;
+                    userProgress.put(chatId, questionIndex);
+                    if (questionIndex < questions.size()) {
+                        editQuestion(chatId, questionIndex, messageId);
+                    } else {
+                        finishTest(chatId);
+                    }
+                }
+                break;
         }
 
         AnswerCallbackQuery answerCallbackQuery = AnswerCallbackQuery.builder()
@@ -148,15 +315,14 @@ public class Bot extends TelegramLongPollingBot {
         }
     }
 
-    private void editQuestion(Long chatId, int questionIndex) {
+    private void editQuestion(Long chatId, int questionIndex, int messageId) {
         Question question = questions.get(questionIndex);
-        InlineKeyboardMarkup keyboard = buildQuestionKeyboard(question);
 
         EditMessageText editMessageText = EditMessageText.builder()
                 .chatId(chatId.toString())
-                .messageId(userMessages.get(chatId))
+                .messageId(messageId)
                 .text(question.getQuestion())
-                .replyMarkup(keyboard)
+                .replyMarkup(createQuestionMarkup(question))
                 .build();
 
         try {
@@ -192,20 +358,6 @@ public class Bot extends TelegramLongPollingBot {
         }
     }
 
-    public void askCandidateInfo(Long chatId) {
-        SendMessage message = SendMessage.builder()
-                .chatId(chatId.toString())
-                .text("Поздравляем, вы прошли тестирование! Пожалуйста, отправьте свои ФИО и email.")
-                .build();
-
-        try {
-            execute(message);
-        } catch (TelegramApiException e) {
-            e.printStackTrace();
-        }
-    }
-
-
     public void saveCandidateInfo(Message message) {
         String[] info = message.getText().split(",");
         Long chatId = message.getChatId();
@@ -213,12 +365,12 @@ public class Bot extends TelegramLongPollingBot {
             String name = info[0].trim();
             String email = info[1].trim();
 
-            int totalQuestions = questions.size(); // Общее количество вопросов в тесте (здесь может быть ваше значение)
-            int correctAnswers = userScores.get(chatId); // Количество правильных ответов
-            int percent = (int) Math.round((double) correctAnswers / totalQuestions * 100); // Вычисляем процент правильных ответов
+            int totalQuestions = questions.size(); //общее количество вопросов в тесте
+            int correctAnswers = userScores.get(chatId); //количество правильных ответов
+            int percent = (int) Math.round((double) correctAnswers / totalQuestions * 100); //вычисляем процент правильных ответов
 
-            dbManager.saveCandidate(name, email, percent); // Сохраняем процент в базу данных
-            sendAdminNotification(name, email); // Уведомляем администратора
+            dbManager.saveCandidate(name, email, percent); //сохраняем процент в базу данных
+            sendAdminNotification(name, email); //уведомляем администратора
 
             waitingForCandidateInfo.put(chatId, false);
             sendCompletionMessage(chatId);
@@ -292,6 +444,102 @@ public class Bot extends TelegramLongPollingBot {
 
         try {
             execute(message);
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void sendMaterialsMenu(Long chatId, int messageId) {
+        List<List<InlineKeyboardButton>> materialsButtons = new ArrayList<>();
+        materialsButtons.add(List.of(
+                InlineKeyboardButton.builder()
+                        .text("Вводный модуль")
+                        .callbackData("вводный модуль")
+                        .build()
+        ));
+        materialsButtons.add(List.of(
+                InlineKeyboardButton.builder()
+                        .text("Инкапсуляция")
+                        .callbackData("инкапсуляция")
+                        .build()
+        ));
+        materialsButtons.add(List.of(
+                InlineKeyboardButton.builder()
+                        .text("Числа и даты")
+                        .callbackData("числа и даты")
+                        .build()
+        ));
+        materialsButtons.add(List.of(
+                InlineKeyboardButton.builder()
+                        .text("Строки")
+                        .callbackData("строки")
+                        .build()
+        ));
+        materialsButtons.add(List.of(
+                InlineKeyboardButton.builder()
+                        .text("Массивы и списки")
+                        .callbackData("массивы и списки")
+                        .build()
+        ));
+        materialsButtons.add(List.of(
+                InlineKeyboardButton.builder()
+                        .text("Коллекции Set, Map")
+                        .callbackData("коллекции Set, Map")
+                        .build()
+        ));
+        materialsButtons.add(List.of(
+                InlineKeyboardButton.builder()
+                        .text("Comparator Iterator Collections")
+                        .callbackData("comparator Iterator Collections")
+                        .build()
+        ));
+        materialsButtons.add(List.of(
+                InlineKeyboardButton.builder()
+                        .text("Назад")
+                        .callbackData("назад")
+                        .build()
+        ));
+
+        InlineKeyboardMarkup materialsMarkup = InlineKeyboardMarkup.builder()
+                .keyboard(materialsButtons)
+                .build();
+
+        EditMessageText editMessageText = EditMessageText.builder()
+                .chatId(chatId.toString())
+                .messageId(messageId)
+                .text("Выберите материал:")
+                .build();
+
+        EditMessageReplyMarkup editMessageReplyMarkup = EditMessageReplyMarkup.builder()
+                .chatId(chatId.toString())
+                .messageId(messageId)
+                .replyMarkup(materialsMarkup)
+                .build();
+
+        try {
+            execute(editMessageText);
+            execute(editMessageReplyMarkup);
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void sendMaterialDetails(Long chatId, int messageId, String text, InlineKeyboardMarkup markup) {
+        EditMessageText editMessageText = EditMessageText.builder()
+                .chatId(chatId.toString())
+                .messageId(messageId)
+                .text(text)
+                .build();
+
+        EditMessageReplyMarkup editMessageReplyMarkup = EditMessageReplyMarkup.builder()
+                .chatId(chatId.toString())
+                .messageId(messageId)
+                .replyMarkup(markup)
+                .build();
+
+        try {
+            execute(editMessageText);
+            execute(editMessageReplyMarkup);
         } catch (TelegramApiException e) {
             e.printStackTrace();
         }
